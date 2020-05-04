873065cd9897a1cd269dee615ceee9f9578bb505
Aleksey-AV <aleksey.webmake@gmail.com>
2020-05-04 15:05:23 +0300

    1

---
+++ routes/patients.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const { Patient } = require('../models/Patient');
+
+router.post('/', async (req, res) => {
+  const questions = await Question.find()
+  try {
+    res.json({
+      questions
+    })
+  } catch (error) {
+    res.json({
+      error
+    })
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  const id = req.params.id
+  const question = await Question.findById({ _id: id })
+  try {
+    res.json({
+      question
+    })
+  } catch (error) {
+    res.json({
+      error
+    })
+  }
+})
+
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+const { User } = require('../models/User');
+const auth = require('../middleware/auth')
+
+router.get('/', auth, (req, res) => {
+  res.render('')
+})
+
+
+router.get('/all', async (req, res) => {
+  const users = await User.find()
+  try {
+    res.json({
+      users
+    })
+  } catch (error) {
+    res.json({
+      error
+    })
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  const id = req.params.id
+  const user = await User.findById({ _id: id })
+  try {
+    res.json({
+      user
+    })
+  } catch (error) {
+    res.json({
+      error
+    })
+  }
+})
+
+module.exports = router;
+
